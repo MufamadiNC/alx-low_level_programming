4e4ef78d34e9127a13786592489fe0830d7d6a7e
MufamadiNC <ndango.mufamadi@hotmail.com>
2023-09-10 13:14:33 -0700

    C Variables and loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 
 	if (mod > 5)
 	{
-		printf("Last digit of %d is %d is greater than 5\n", n, mod);
+		printf("Last digit of %d is %d and is greater than 5\n", n, mod);
 	}
 	if (mod == 0)
 	{
