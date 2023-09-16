3297284d3aa74d537ebaf350b36b5cba933faf90
MufamadiNC <ndango.mufamadi@hotmail.com>
2023-09-16 08:54:49 -0700

    more Nested functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer value
+ * @b: integer value
+ *
+ * Return: integer value a*b
+ */
+
+int mul(signed int a, signed int b)
+{
+	return (a * b);
+}
