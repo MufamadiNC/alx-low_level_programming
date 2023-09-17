560e35bd34608e9b669eee5b38a162f6c876a3ff
MufamadiNC <ndango.mufamadi@hotmail.com>
2023-09-17 04:37:16 -0700

    more Nested Functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,11 +13,12 @@ void print_line(int n)
 	{
 		_putchar('\n');
 	}
-	else
+	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
 	}
 }
