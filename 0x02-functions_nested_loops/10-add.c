098202cdabbb1e5edfae47468917f7ee7af80120
MufamadiNC <ndango.mufamadi@hotmail.com>
2023-09-17 22:00:29 -0700

    more Nested functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of two integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
