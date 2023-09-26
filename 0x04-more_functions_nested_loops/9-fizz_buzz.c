1851be3955b882d4bbb6a4f7cd52fd8badf15028
MufamadiNC <ndango.mufamadi@hotmail.com>
2023-09-26 05:32:18 -0700

    Fizz Buzz interview test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100
+ * multiples of 3 are printed as 'Fizz'
+ * multiplies of 5 are printed as 'Buzz'
+ * multiples 3 and 5 are printed as 'FizzBuzz'
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+}
